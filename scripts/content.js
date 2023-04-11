8d53ddb530a4ab9303a4b0fcfa42545ebab1ba4d
Lucas Detling <lucas.detling@gmail.com>
2023-04-11 16:14:31 +0200

    modifs rapid

--- scripts/content.js
+++ scripts/content.js
@@ -1,23 +1,21 @@
 window.onload = (event) => {
     const inventoryLocation = "https://www.twitch.tv/drops/inventory";
-    if(window.location.href === inventoryLocation){
+    if (window.location.href === inventoryLocation) {
         const previousUrlTwitch = sessionStorage.getItem("previousUrlTwitch");
         setInterval(() => {
-                const contentDrops = document.querySelectorAll("[data-test-selector='DropsCampaignInProgressRewardPresentation-claim-button']");
-            if(contentDrops.length > 0) {
-                for(let drop of contentDrops){
+            const contentDrops = document.querySelectorAll("[data-test-selector='DropsCampaignInProgressRewardPresentation-claim-button']");
+            if (contentDrops.length > 0) {
+                for (let drop of contentDrops) {
                     drop.click();
                 }
                 return
-            } 
+            }
             else {
                 window.location.href = previousUrlTwitch;
             }
-    }, 1000);
-    return;
-} else {
-    //if notification ok 
-    sessionStorage.setItem("previousUrlTwitch", window.location.href);
-}
-
+        }, 1000);
+    } else {
+        //if notification ok 
+        sessionStorage.setItem("previousUrlTwitch", window.location.href);
+    }
 }

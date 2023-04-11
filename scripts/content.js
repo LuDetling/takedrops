670d4d8922372088ca86430c3a4f0694ffe45012
Lucas Detling <lucas.detling@gmail.com>
2023-04-11 13:23:56 +0200

    return last page after take drops

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,23 @@
+window.onload = (event) => {
+    const inventoryLocation = "https://www.twitch.tv/drops/inventory";
+    if(window.location.href === inventoryLocation){
+        const previousUrlTwitch = sessionStorage.getItem("previousUrlTwitch");
+        setInterval(() => {
+                const contentDrops = document.querySelectorAll("[data-test-selector='DropsCampaignInProgressRewardPresentation-claim-button']");
+            if(contentDrops.length > 0) {
+                for(let drop of contentDrops){
+                    drop.click();
+                }
+                return
+            } 
+            else {
+                window.location.href = previousUrlTwitch;
+            }
+    }, 1000);
+    return;
+} else {
+    //if notification ok 
+    sessionStorage.setItem("previousUrlTwitch", window.location.href);
+}
 
-setInterval(() => {
-    console.log("test");
-    const contentDrops = document.querySelectorAll("[data-test-selector='DropsCampaignInProgressRewardPresentation-claim-button']");
-    if(contentDrops.length > 0) {
-        for(let drop of contentDrops){
-            drop.click();
-        }
-        return
-    } 
-    else {
-        console.log("nothing to take");
-    }
-}, 10000 * 6);
+}

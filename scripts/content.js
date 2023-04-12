027bcbd13f371a52dfecae8b465d9eb39a18b180
Lucas Detling <lucas.detling@gmail.com>
2023-04-12 22:29:12 +0200

    timer + comments + initial sessionStorage

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,16 @@
-window.onload = (event) => {
+window.onload = () => {
     const inventoryLocation = "https://www.twitch.tv/drops/inventory";
+    //initial session storage 
+    if (!sessionStorage.getItem("previousUrlTwitch")) {
+        sessionStorage.setItem("previousUrlTwitch", JSON.stringify({
+            url: "https://www.twitch.tv",
+            isRedirect: false,
+        }));
+    }
     const { isRedirect, url } = JSON.parse(sessionStorage.getItem("previousUrlTwitch"));
 
     if (window.location.href === inventoryLocation && isRedirect === true) {
+        //click on all the drops and close popup
         setInterval(() => {
             const contentDrops = document.querySelectorAll("[data-test-selector='DropsCampaignInProgressRewardPresentation-claim-button']");
             if (contentDrops.length > 0) {
@@ -15,22 +23,25 @@ window.onload = (event) => {
                 sessionStorage.setItem("previousUrlTwitch", JSON.stringify({ url, isRedirect: false }));
                 window.close()
             }
-        }, 5000);
+        }, 3000);
     } else {
-        //if notification ok 
-        let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=1,height=1,left=-1000,top=-1000`;
+        let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1,height=1,left=-1000,top=-1000`;
         sessionStorage.setItem("previousUrlTwitch", JSON.stringify({ url: window.location.href, isRedirect: false }));
         setInterval(() => {
             const notification = document.querySelector(".onsite-notifications__badge");
             const openNotif = document.querySelector(".onsite-notifications button")
-            if (!notification) return
+
+            if (!notification) return;
+
+            //click to reload notif at 0
             openNotif.click();
-            sessionStorage.setItem("previousUrlTwitch", JSON.stringify({ url, isRedirect: true }));
             setTimeout(() => {
-                window.open(inventoryLocation, "inventory", params);
+                openNotif.click();
             }, 1000);
 
+            //set is redirect at true and open popup inventory
+            sessionStorage.setItem("previousUrlTwitch", JSON.stringify({ url, isRedirect: true }));
+            window.open(inventoryLocation, "inventory", params);
         }, 10000 * 6);
     }
 }
